af05f9a1954f0487decf56fdbe5d8ae980348163
Evgeniy Abramov <jekaabramov@bk.ru>
2024-02-11 20:48:03 +0300

    1

---
+++ terraform/locals.tf
@@ -0,0 +1,4 @@
+locals {
+  name_web = "netology-${var.vpc_name}-v"
+  name_db = "netology-${var.vpc_name}-ch"
+}
---
+++ terraform/vms_platform.tf
@@ -0,0 +1,44 @@
+### vm web
+
+variable "vm_web_image_family" {
+  type = string
+  default = "centos-7"
+}
+
+variable "vm_web_platform_id" {
+  type = string
+  default = "standard-v3"
+}
+
+### vm_db
+
+variable "vm_db_image_family" {
+  type = string
+  default = "centos-7"
+}
+
+variable "vm_db_platform_id" {
+  type = string
+  default = "standard-v3"
+}
+
+# VM web resources
+variable "vm_web_resources" {
+  type = map(number)
+  default = {
+    cores         = 2
+    memory        = 1
+    core_fraction = 20
+  }
+}
+
+# VM db resources
+variable "vm_db_resources" {
+  type = map(number)
+  default = {
+    cores         = 2
+    memory        = 1
+    core_fraction = 20
+  }
+}
+
